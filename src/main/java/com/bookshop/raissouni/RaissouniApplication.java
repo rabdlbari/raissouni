d0f08c7d70c8588833cb77be22c0ea3fd3b65ead
[Abdelbarie Raissouni] <rabdlbari@gmail.com>
2026-02-21 09:38:11 +0000

    init commit

---
+++ src/main/java/com/bookshop/raissouni/RaissouniApplication.java
@@ -0,0 +1,13 @@
+package com.bookshop.raissouni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaissouniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaissouniApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bookshop/raissouni/RaissouniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookshop.raissouni;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaissouniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
