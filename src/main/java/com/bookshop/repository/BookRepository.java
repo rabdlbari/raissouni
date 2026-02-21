31942c3f77eae1e9fcf5ed04907f304ab8a6694a
oumaima <oumaima.ajana@etu.uae.ac.ma>
2026-02-21 12:15:05 +0000

    repository

---
+++ src/main/java/com/bookshop/repository/BookRepository.java
@@ -0,0 +1,12 @@
+package com.bookshop.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Repository;
+import com.bookshop.entity.Book; 
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Long> {
+    // Pagination is required for the public API
+    Page<Book> findAll(Pageable pageable); 
+}
